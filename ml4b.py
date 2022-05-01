95e299d7b4fecf732056c69f8ae3a93a3514300c
Cookiegraysy <50171488+Cookiegraysy@users.noreply.github.com>
2022-05-01 12:34:44 +0200

    Add files via upload

---
+++ ml4b.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title('Garbage Classification')
+
+
+st.markdown('Hello, we are Jan, Narin and Siyu. Our project is to train a model to classify images of garbage correctly '
+          'and compare different approaches on different dataset sizes.', True)
+
