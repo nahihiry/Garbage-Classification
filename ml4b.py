1c3b617cc150e0e1b5818b84c5d92178580713fd
Cookiegraysy <50171488+Cookiegraysy@users.noreply.github.com>
2022-05-01 13:11:35 +0200

    Add files via upload

--- ml4b.py
+++ ml4b.py
@@ -9,3 +9,10 @@ st.title('Garbage Classification')
 st.markdown('Hello, we are Jan, Narin and Siyu. Our project is to train a model to classify images of garbage correctly '
           'and compare different approaches on different dataset sizes.', True)
 
+# Example cardboard garbage image
+from PIL import Image
+image = Image.open('cardboard1.jpg')
+
+st.image(image, caption='Example cardboard garbage image')
+
+uploaded_file = st.file_uploader("Choose an image...")
