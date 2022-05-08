c4877d7663dd72c70b6b19aa1f26f1dddf30546d
nahihiry <92831472+nahihiry@users.noreply.github.com>
2022-05-08 13:44:09 +0200

    Update ml4b.py

--- ml4b.py
+++ ml4b.py
@@ -6,7 +6,7 @@ import numpy as np
 st.title('Garbage Classification')
 
 with st.expander("About us:")
-st.write("Hello, we are Jan, Narin and Siyu. We are student of business informatics and are happy to attend the course ML4B and learn more about the world oft machine learning.")
+st.write("Hello, we are Jan, Narin and Siyu. We are students of business informatics and are glad to be able to attend the course ML4B and learn more about the world of machine learning.")
 
 
 with st.expander ("Our Goal:")
