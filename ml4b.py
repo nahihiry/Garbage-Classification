6408d4b25e09b3e61a68e34c3f9259f201587345
Cookiegraysy <50171488+Cookiegraysy@users.noreply.github.com>
2022-05-23 18:07:46 +0200

    Update ml4b.py

--- ml4b.py
+++ ml4b.py
@@ -1,24 +1,21 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import opendatasets as od
-
 
 st.title('Garbage Classification')
 
-dataset = "https://www.kaggle.com/datasets/asdasdasasdas/garbage-classification?resource=download"
-od.download(dataset)
 
-st.markdown('Hello, we are Jan, Narin and Siyu', True)
+with st.expander("About us:"):
+    st.write(
+        "Hello, we are Jan, Narin and Siyu. We are student of business informatics and are happy to attend the course ML4B and learn more about the world oft machine learning.")
 
-with st.expander ("The problem:")
-st.write("Many people dont know how to seperate their waste correctly. Most of the countries dont have a law that requires their inhabitants to seperate their trash, which is why lots of people dont even know how recycling works. Recycling, however, is an important point in order to protect the environment.") 
-
-with st.expander ("Our Goal:")
-st. write ("Our project is to train a model to classify images of garbage correctly and compare different approaches on different dataset sizes. The purpose of our app is to to separate waste properly. Our dataset is divided into six categories: cardboard, glass, metal, paper, plastic and a test dataset. We  
+with st.expander("Our Goal:"):
+    st.write(
+        "Our project is to train a model to classify images of garbage correctly and compare different approaches on different dataset sizes.")
 
 # Example cardboard garbage image
 from PIL import Image
+
 image = Image.open('cardboard1.jpg')
 
 st.image(image, caption='Example cardboard garbage image')
