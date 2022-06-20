a769850f43bc21002b930fbdd3aba80a5c9b654e
Cookiegraysy <chensiyu96@foxmail.com>
2022-06-20 22:35:05 +0200

    dataset and model

--- ml4b.py
+++ ml4b.py
@@ -1,18 +1,74 @@
+import time
 import streamlit as st
-import pandas as pd
 import numpy as np
 
+from PIL import Image
+import urllib.request
+from utils import *
+
+labels = gen_labels()
+
+
+dir = './Dataset/'
 
 st.title('Garbage Classification')
 
+with st.expander("About us:"):
+    st.write(
+        "Hello, we are Jan, Narin and Siyu. We are student of business informatics and are happy to attend the course ML4B and learn more about the world of machine learning.")
 
-st.markdown('Hello, we are Jan, Narin and Siyu. Our project is to train a model to classify images of garbage correctly '
-          'and compare different approaches on different dataset sizes.', True)
+with st.expander("Our Goal:"):
+    st.write(
+        "Our project is to train a model to classify images of garbage correctly and compare different approaches on different dataset sizes.")
 
-# Example cardboard garbage image
-from PIL import Image
-image = Image.open('cardboard1.jpg')
+html_temp = '''
+    <div style =  padding-bottom: 20px; padding-top: 20px; padding-left: 5px; padding-right: 5px">
+    <center><h1>Garbage Segregation</h1></center>
 
-st.image(image, caption='Example cardboard garbage image')
+    </div>
+    '''
 
-uploaded_file = st.file_uploader("Choose an image...")
+st.markdown(html_temp, unsafe_allow_html=True)
+html_temp = '''
+    <div>
+    <h2></h2>
+    <center><h3>Please upload Waste Image to find its Category</h3></center>
+    </div>
+    '''
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.markdown(html_temp, unsafe_allow_html=True)
+opt = st.selectbox("How do you want to upload the image for classification?\n",
+                   ('Please Select', 'Upload image via link', 'Upload image from device'))
+if opt == 'Upload image from device':
+    file = st.file_uploader('Select', type=['jpg', 'png', 'jpeg'])
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+    if file is not None:
+        image = Image.open(file)
+
+elif opt == 'Upload image via link':
+
+    try:
+        img = st.text_input('Enter the Image Address')
+        image = Image.open(urllib.request.urlopen(img))
+
+    except:
+        if st.button('Submit'):
+            show = st.error("Please Enter a valid Image Address!")
+            time.sleep(4)
+            show.empty()
+
+try:
+    if image is not None:
+        st.image(image, width=300, caption='Uploaded Image')
+        if st.button('Predict'):
+            img = preprocess(image)
+
+            model = model_arc()
+            model.load_weights("../weights/model.h5")
+
+            prediction = model.predict(img[np.newaxis, ...])
+            st.info('Hey! The uploaded image has been classified as " {} waste " '.format(
+                labels[np.argmax(prediction[0], axis=-1)]))
+except:
+    pass
+
