ee0f131ad129f27061f13a1dfbdfb17404137b58
Cookiegraysy <50171488+Cookiegraysy@users.noreply.github.com>
2022-06-21 02:33:36 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ from keras.layers import Conv2D, Flatten, MaxPooling2D, Dense, Dropout, SpatialD
 from tensorflow.keras.losses import sparse_categorical_crossentropy, binary_crossentropy
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+from PIL import Image
 
 def gen_labels():
     train = '../Dataset/Train'
