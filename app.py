ca4e3255f2f4c6c164462133e6bca3c512484d8d
Cookiegraysy <50171488+Cookiegraysy@users.noreply.github.com>
2022-06-24 16:38:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def local_css(file_name):
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-local_css("style/style.css")
+local_css("style.css")
 
 # Load Animation
 animation_symbol1 = "♻️"
